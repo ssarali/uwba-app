c600bdf63a21320316dc972734ff1eae0dddbb79
Aaizakh <aaizakhalid14@gmail.com>
2019-11-04 21:07:16 -0500

    Tips page basic layout

--- src/app/tips/tips.component.ts
+++ src/app/tips/tips.component.ts
@@ -12,4 +12,61 @@ export class TipsComponent implements OnInit {
   ngOnInit() {
   }
 
+  accommodationDropdown = false;
+  selectedLink: string = '';
+
+  accommodations = [
+    [ 
+      "one",
+      "two",
+      "three",
+      "four"
+    ],
+    [
+      "one",
+      "two",
+      "three",
+      "helo"
+    ], 
+    [
+      "one",
+      "two",
+      "three",
+      "hk"
+    ],
+    [
+      "one",
+      "two",
+      "three",
+      "iylusdgh"
+    ]
+  ]
+
+  showAccommodationDropDown(){
+    this.accommodationDropdown = !this.accommodationDropdown;
+  }
+
+  showLinks(event:any){
+    this.selectedLink = event.target.value;
+  }
+
+  flightsDropdown = false;
+
+  showFlightsDropdown(){
+    this.flightsDropdown = !this.flightsDropdown;
+  }
+
+  tourismdropDown = false;
+
+  showTourismDropdown(){
+    this.tourismdropDown = !this.tourismdropDown;
+  }
+
+  visaDropdown = false;
+
+  showVisaDropdown(){
+    this.visaDropdown = !this.visaDropdown;
+  }
+
+
 }
