9cb09a2318f30be05539cea6c29aa9e6fad13dcb
Aaizakh <aaizakhalid14@gmail.com>
2019-11-07 20:39:30 -0500

    Tips  structure & accommodation section complete

--- src/app/tips/tips.component.ts
+++ src/app/tips/tips.component.ts
@@ -15,32 +15,152 @@ export class TipsComponent implements OnInit {
   accommodationDropdown = false;
   selectedLink: string = '';
 
-  accommodations = [
-    [ 
-      "one",
-      "two",
-      "three",
-      "four"
-    ],
-    [
-      "one",
-      "two",
-      "three",
-      "helo"
-    ], 
-    [
-      "one",
-      "two",
-      "three",
-      "hk"
-    ],
-    [
-      "one",
-      "two",
-      "three",
-      "iylusdgh"
+    hostelsList = [
+       [
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hotelscombined.png",
+            "websiteUrl": "https://www.hotelscombined.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelbookers.jpg",
+            "websiteUrl": "https://www.hostelbookers.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelworld.png",
+            "websiteUrl": "https://www.hostelworld.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelz.png",
+            "websiteUrl": "https://www.hostelz.com/"
+          }
+       ],
+       [
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelsclub.png",
+            "websiteUrl": "https://www.hostelsclub.com/en/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostellinginternational.jpg",
+            "websiteUrl": "https://www.hihostels.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelscentral.png",
+            "websiteUrl": "https://www.hostelscentral.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hicanada.png",
+            "websiteUrl": "https://hihostels.ca/en/destinations/ontario/hi-toronto"
+          }
+        ],
+        [
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostels.png",
+            "websiteUrl": "http://www.hostels.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelgeeks.png",
+            "websiteUrl": "https://hostelgeeks.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/couchsurfing.png",
+            "websiteUrl": "https://www.couchsurfing.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/globalfreeloaders.jpg",
+            "websiteUrl": "http://globalfreeloaders.com/"
+          }
+        ],
+        [
+          {
+            "imageUrl": "assets/photos/tips/accommodations/priceline.jpg",
+            "websiteUrl": "https://www.priceline.com//"
+          }
+        ],
+     ];
+
+     hotelsList = [
+      [
+         {
+           "imageUrl": "assets/photos/fb_logo.png",
+           "websiteUrl": "https://www.booking.com/"
+         },
+         {
+           "imageUrl": "2",
+           "websiteUrl": "helOOOOOOO"
+         },
+         {
+           "imageUrl": "1",
+           "websiteUrl": "helOOOOOOO"
+         },
+         {
+           "imageUrl": "2",
+           "websiteUrl": "helOOOOOOO"
+         }
+      ],
+      [
+         {
+           "imageUrl": "1",
+           "websiteUrl": "helOOOOOOO"
+         },
+         {
+           "imageUrl": "2",
+           "websiteUrl": "helOOOOOOO"
+         },
+         {
+           "imageUrl": "1",
+           "websiteUrl": "helOOOOOOO"
+         },
+         {
+           "imageUrl": "2",
+           "websiteUrl": "helOOOOOOO"
+         }
+       ],
+       [
+        {
+          "imageUrl": "excuse",
+          "websiteUrl": "helOOOOOOO"
+        },
+       ]
+    ];
+
+    airbnbList = [
+      [
+         {
+           "imageUrl": "1",
+           "websiteUrl": "helOOOOOOO"
+         },
+         {
+           "imageUrl": "2",
+           "websiteUrl": "helOOOOOOO"
+         },
+         {
+           "imageUrl": "1",
+           "websiteUrl": "helOOOOOOO"
+         },
+         {
+           "imageUrl": "2",
+           "websiteUrl": "helOOOOOOO"
+         }
+      ],
+      [
+         {
+           "imageUrl": "1",
+           "websiteUrl": "helOOOOOOO"
+         },
+         {
+           "imageUrl": "2",
+           "websiteUrl": "helOOOOOOO"
+         },
+         {
+           "imageUrl": "1",
+           "websiteUrl": "helOOOOOOO"
+         },
+         {
+           "imageUrl": "2",
+           "websiteUrl": "helOOOOOOO"
+         }
+       ]
     ]
-  ]
 
   showAccommodationDropDown(){
     this.accommodationDropdown = !this.accommodationDropdown;
