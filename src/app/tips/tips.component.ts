86a5639143672a0d76b471196f011b722925473f
Aaizakh <aaizakhalid14@gmail.com>
2019-11-18 23:11:03 -0500

    Subcategory section deleted and code restructured

--- src/app/tips/tips.component.ts
+++ src/app/tips/tips.component.ts
@@ -12,10 +12,40 @@ export class TipsComponent implements OnInit {
   ngOnInit() {
   }
 
-  accommodationDropdown = false;
-  selectedLink: string = '';
+  showAccommodation = false;
+  showFlight = false;
+  showTourism = false;
+  showVisa = false;
 
-    hostelsList = [
+  showList(id:any){
+    if (id == 0){
+        this.showAccommodation = true;
+        this.showFlight = false;
+        this.showTourism = false;
+        this.showVisa = false;
+    }
+    else if (id == 1){ 
+        this.showAccommodation = false;
+        this.showFlight = true;
+        this.showTourism = false;
+        this.showVisa = false;
+    }
+    else if (id == 2){
+        this.showAccommodation = false;
+        this.showFlight = false;
+        this.showTourism = true;
+        this.showVisa = false;
+    }
+    else if (id == 3){
+        this.showAccommodation = false;
+        this.showFlight = false;
+        this.showTourism = false;
+        this.showVisa = true;
+    }
+  } 
+
+
+  accommodationList = [
        [
           {
             "imageUrl": "assets/photos/tips/accommodations/hostelscombined.png",
@@ -72,121 +102,167 @@ export class TipsComponent implements OnInit {
         ],
         [
           {
-            "imageUrl": "assets/photos/tips/accommodations/priceline.jpg",
-            "websiteUrl": "https://www.priceline.com//"
+            "imageUrl": "assets/photos/tips/accommodations/hostelscombined.png",
+            "websiteUrl": "https://www.hotelscombined.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelbookers.jpg",
+            "websiteUrl": "https://www.hostelbookers.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelworld1.png",
+            "websiteUrl": "https://www.hostelworld.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelz.png",
+            "websiteUrl": "https://www.hostelz.com/"
+          }
+       ],
+       [
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelsclub.png",
+            "websiteUrl": "https://www.hostelsclub.com/en/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostellinginternational.jpg",
+            "websiteUrl": "https://www.hihostels.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelscentral.png",
+            "websiteUrl": "https://www.hostelscentral.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hicanada1.png",
+            "websiteUrl": "https://hihostels.ca/en/destinations/ontario/hi-toronto"
+          }
+        ],
+        [
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostels.png",
+            "websiteUrl": "http://www.hostels.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelgeeks.png",
+            "websiteUrl": "https://hostelgeeks.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/couchsurfing.png",
+            "websiteUrl": "https://www.couchsurfing.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/globalfreeloaders.jpg",
+            "websiteUrl": "http://globalfreeloaders.com/"
+          }
+        ],
+        [
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelscombined.png",
+            "websiteUrl": "https://www.hotelscombined.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelbookers.jpg",
+            "websiteUrl": "https://www.hostelbookers.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelworld1.png",
+            "websiteUrl": "https://www.hostelworld.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelz.png",
+            "websiteUrl": "https://www.hostelz.com/"
+          }
+       ],
+       [
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelsclub.png",
+            "websiteUrl": "https://www.hostelsclub.com/en/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostellinginternational.jpg",
+            "websiteUrl": "https://www.hihostels.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelscentral.png",
+            "websiteUrl": "https://www.hostelscentral.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hicanada1.png",
+            "websiteUrl": "https://hihostels.ca/en/destinations/ontario/hi-toronto"
+          }
+        ],
+        [
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostels.png",
+            "websiteUrl": "http://www.hostels.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelgeeks.png",
+            "websiteUrl": "https://hostelgeeks.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/couchsurfing.png",
+            "websiteUrl": "https://www.couchsurfing.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/globalfreeloaders.jpg",
+            "websiteUrl": "http://globalfreeloaders.com/"
+          }
+        ],
+        [
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelscombined.png",
+            "websiteUrl": "https://www.hotelscombined.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelbookers.jpg",
+            "websiteUrl": "https://www.hostelbookers.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelworld1.png",
+            "websiteUrl": "https://www.hostelworld.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelz.png",
+            "websiteUrl": "https://www.hostelz.com/"
+          }
+       ],
+       [
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelsclub.png",
+            "websiteUrl": "https://www.hostelsclub.com/en/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostellinginternational.jpg",
+            "websiteUrl": "https://www.hihostels.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelscentral.png",
+            "websiteUrl": "https://www.hostelscentral.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hicanada1.png",
+            "websiteUrl": "https://hihostels.ca/en/destinations/ontario/hi-toronto"
+          }
+        ],
+        [
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostels.png",
+            "websiteUrl": "http://www.hostels.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/hostelgeeks.png",
+            "websiteUrl": "https://hostelgeeks.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/couchsurfing.png",
+            "websiteUrl": "https://www.couchsurfing.com/"
+          },
+          {
+            "imageUrl": "assets/photos/tips/accommodations/globalfreeloaders.jpg",
+            "websiteUrl": "http://globalfreeloaders.com/"
           }
         ],
      ];
-
-     hotelsList = [
-      [
-         {
-           "imageUrl": "assets/photos/fb_logo.png",
-           "websiteUrl": "https://www.booking.com/"
-         },
-         {
-           "imageUrl": "2",
-           "websiteUrl": "helOOOOOOO"
-         },
-         {
-           "imageUrl": "1",
-           "websiteUrl": "helOOOOOOO"
-         },
-         {
-           "imageUrl": "2",
-           "websiteUrl": "helOOOOOOO"
-         }
-      ],
-      [
-         {
-           "imageUrl": "1",
-           "websiteUrl": "helOOOOOOO"
-         },
-         {
-           "imageUrl": "2",
-           "websiteUrl": "helOOOOOOO"
-         },
-         {
-           "imageUrl": "1",
-           "websiteUrl": "helOOOOOOO"
-         },
-         {
-           "imageUrl": "2",
-           "websiteUrl": "helOOOOOOO"
-         }
-       ],
-       [
-        {
-          "imageUrl": "excuse",
-          "websiteUrl": "helOOOOOOO"
-        },
-       ]
-    ];
-
-    airbnbList = [
-      [
-         {
-           "imageUrl": "1",
-           "websiteUrl": "helOOOOOOO"
-         },
-         {
-           "imageUrl": "2",
-           "websiteUrl": "helOOOOOOO"
-         },
-         {
-           "imageUrl": "1",
-           "websiteUrl": "helOOOOOOO"
-         },
-         {
-           "imageUrl": "2",
-           "websiteUrl": "helOOOOOOO"
-         }
-      ],
-      [
-         {
-           "imageUrl": "1",
-           "websiteUrl": "helOOOOOOO"
-         },
-         {
-           "imageUrl": "2",
-           "websiteUrl": "helOOOOOOO"
-         },
-         {
-           "imageUrl": "1",
-           "websiteUrl": "helOOOOOOO"
-         },
-         {
-           "imageUrl": "2",
-           "websiteUrl": "helOOOOOOO"
-         }
-       ]
-    ]
-
-  showAccommodationDropDown(){
-    this.accommodationDropdown = !this.accommodationDropdown;
-  }
-
-  showLinks(event:any){
-    this.selectedLink = event.target.value;
-  }
-
-  flightsDropdown = false;
-
-  showFlightsDropdown(){
-    this.flightsDropdown = !this.flightsDropdown;
-  }
-
-  tourismdropDown = false;
-
-  showTourismDropdown(){
-    this.tourismdropDown = !this.tourismdropDown;
-  }
-
-  visaDropdown = false;
-
-  showVisaDropdown(){
-    this.visaDropdown = !this.visaDropdown;
-  }
 
 
 }
